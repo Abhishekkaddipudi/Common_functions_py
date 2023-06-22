fbd8ffc70d5a3280fcb45cfbf44dfca9f9157e31
RohitJadar <rohitjadar880@gmail.com>
2023-06-22 21:42:53 +0530

    new

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 from typing import List
 REQUIREMENT_FILE_NAME="requirements.txt"
 Project_Name="PYPI PUBLISHING"
-Version="0.0.1"
+Version="0.0.0.1"
 AUTHOR="Abhishek"
 DESCRIPTION="Experiment For publishing"
 PACKAGES=["PYPI PUBLISHING"]
